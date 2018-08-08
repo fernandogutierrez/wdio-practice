0cd135faeb7a3262b5768b115d96ca5de22739cd
Fernando Gutierrez <fernando.gutierrez@jalasoft.com>
2018-08-08 16:49:33 -0400

    Added steps to when.js to insert username and email

--- src/step_definitions/when.js
+++ src/step_definitions/when.js
@@ -1,14 +1,14 @@
 const { When } = require('cucumber');
+const contactPage = require('../pageobjects/contact');
 
-When(/^I insert '(.*)'$/, function (string) {
-
+When(/^I insert '(.*)'$/, function (name) {
+   contactPage.set_name(name)
 });
 
 When(/^I insert an invalid email$/, function () {
-
+   contactPage.set_email('somerandomtext')
 });
 
-
 When(/^I press on Send$/, function () {
-
+    contactPage.submit_form();
 });
